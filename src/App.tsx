d0e7d1184467bdbf30af777d715e981be65f911c
rbuzatto <mightyrafa@gmail.com>
2022-06-28 21:04:50 -0300

    feat: add chackra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,27 @@
-import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
+import { ChakraProvider, Heading, Container, Box } from "@chakra-ui/react"
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = (): JSX.Element => {
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <ChakraProvider>
+      <main className="App">
+        <Container>
+          <Box boxShadow='xl' p='24' rounded='md' bg='white'>
+            <header className="App-header">
+              <Heading
+                bgGradient='linear(to-l, #7928CA, #bfcf31cf)'
+                bgClip='text'
+                as='h1' size='2xl'
+                fontWeight='extrabold'
+              >
+                Welcome to Chakra UI
+              </Heading>
+            </header>
+          </Box>
+        </Container>
+      </main>
+    </ChakraProvider>
   )
 }
 
